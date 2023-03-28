9072d45341aac529d19aede07db7e6c508391d1a
fuzionu <fuzionu@gmail.com>
2023-03-28 20:51:27 +0200

    refactor code, update gitignore

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import random
 
-top_of_range = input("Type a number: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+def main():
+    answer = input("Hello! Do you want to play? YES/NO\n> ")
 
-    if top_of_range <= 0:
-        print("Please type a number larger than 0!")
-        quit()
+    if answer.lower() != "yes":
+        print("Good bye!")
+    else:
+        start_game()
 
-else:
-    print("Please type a number next time!")
-    quit()
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
+def start_game():
+    top_of_range = input("Type a number: ")
+    if top_of_range.isdigit():
+        top_of_range = int(top_of_range)
 
-while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
+        if top_of_range <= 0:
+            print("Please type a number larger than 0!")
+            quit()
 
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
     else:
         print("Please type a number next time!")
-        continue
+        quit()
+    random_number = random.randint(0, top_of_range)
+    guesses = 0
+    while True:
+        guesses += 1
+        user_guess = input("Make a guess: ")
 
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+        else:
+            print("Please type a number next time!")
+            continue
+
+        if user_guess == random_number:
+            print("You got it!")
+            break
+        elif user_guess > random_number:
             print("You were above the number!")
-    else:
-        print("You were below the number!")
+        else:
+            print("You were below the number!")
+    print("You got it in", guesses, "guesses")
 
-print("You got it in", guesses, "guesses")
-
+if __name__ == '__main__':
+    main()
